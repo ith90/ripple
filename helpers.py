fed7649b349ccd39f1ca3cedcf5b27e5201bb00b
Isa <isa.he@outlook.com>
2023-12-10 18:31:29 -0500

    moved hugging face classifer out of function

--- helpers.py
+++ helpers.py
@@ -34,8 +34,11 @@ nltk.download('stopwords')
 from sqlalchemy import func
 
 
+classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
+
+
 def login_required(f):
-    """
+    ""
     Decorate routes to require login.
 
     http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
@@ -174,7 +177,6 @@ emotion_names = [
 
 # Roberta Hugging face
 def emotions_hf(entry):
-    classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
 
     # data output as lists
     data = classifier(entry)
