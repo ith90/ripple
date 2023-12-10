f9732fbbda2cbc2e812db641eb77633585e1773d
Isa <isa.he@outlook.com>
2023-12-10 17:52:22 -0500

    ...

--- app.py
+++ app.py
@@ -46,7 +46,6 @@ def create_app():
 
 app = create_app()
 if __name__ == "__main__":
-    # Use Heroku's provided port if available
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
     
